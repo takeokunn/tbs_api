ab23f525b1734789327f9a7db66a52d256225f1f
TakeObara <bararararatty@gmail.com>
2016-09-13 22:19:24 +0900

    Create 'stocks' migration file

---
+++ database/migrations/2016_09_13_131234_create_stocks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('program_id');
+            $table->integer('user_id');
+            $table->integer('stock_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('stocks');
+    }
+}
