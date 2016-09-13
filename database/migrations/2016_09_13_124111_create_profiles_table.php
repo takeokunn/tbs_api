07d255ab441f7d9821113f946c9ec9f15bf9cf97
TakeObara <bararararatty@gmail.com>
2016-09-13 22:06:54 +0900

    Create 'profiles' migration file

---
+++ database/migrations/2016_09_13_124111_create_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('profile_img_url');
+            $table->integer('tbs_point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profiles');
+    }
+}
