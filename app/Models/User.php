1eb07f2a937dd590de3aca22098bacc820f4db29
TakeObara <bararararatty@gmail.com>
2016-09-14 21:26:19 +0900

    Update 'user' model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract,
+                                    AuthorizableContract
 {
+    use Authenticatable, Authorizable;
+
     protected $table = 'users';
     protected $hidden = ['password', 'twitter_id', 'facebook_id'];
 
