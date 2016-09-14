de103df010da513eee231730cc1c447213af079a
TakeObara <bararararatty@gmail.com>
2016-09-14 23:23:57 +0900

    Add column 'username' in profile table

---
+++ database/migrations/2016_09_14_142201_alter_table_profiles_add_username.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableProfilesAddUsername extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function ($table) {
+            $table->text('username')->nullable()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function ($table) {
+            $table->dropColumn('username');
+        });
+    }
+}
