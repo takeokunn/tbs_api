7c41ff2d154e1bcc2bcb4a447deafba8044f3ffd
TakeObara <bararararatty@gmail.com>
2016-09-14 18:03:02 +0900

    Create app/Jsons/CommonJson.php

---
+++ app/Jsons/CommonJson.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Jsons;
+
+trait CommonJson
+{
+    /**
+     * json format if invalid request data
+     * @return array
+     */
+    private function invalidArgument() : array
+    {
+        return [
+            'status'  => 'failure',
+            'code'    => 400,
+            'message' => 'invalid argument'
+        ];
+    }
+
+    /**
+     * json format if permission denied
+     * @return array
+     */
+    private function noPermission() : array
+    {
+        return [
+            'status'  => 'failure',
+            'code'    => 403,
+            'message' => 'you have no permission'
+        ];
+    }
+
+    /**
+     * json format if server error occurred
+     * @param  string $error_
+     * @return array
+     */
+    private function serverError(string $error_) : array
+    {
+        return [
+            'status'  => 'error',
+            'code'    => 500,
+            'log'     => $error_,
+            'message' => 'server error'
+        ];
+    }
+}
