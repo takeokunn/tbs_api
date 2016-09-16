ff7a0c76d4b7e44e1c86a8e2a5ba1596bd651972
TakeObara <bararararatty@gmail.com>
2016-09-16 20:42:26 +0900

    Add comment in AppSeeder.php

--- database/seeds/AppSeeder.php
+++ database/seeds/AppSeeder.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Seeder;
 
 class AppSeeder extends Seeder
 {
+    /**
+     * change data id
+     * @param  Object $modelData_
+     * @param  int    $newId_
+     */
     protected function changeId($modelData_, int $newId_)
     {
         $modelData_->id = $newId_;
