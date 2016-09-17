0a9c9d59b6ddd122409f6f343590fac048eea59a
TakeObara <bararararatty@gmail.com>
2016-09-17 17:59:30 +0900

    Create ProgramSaleList model

---
+++ app/Models/ProgramSaleList.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramSaleList extends Model
+{
+    protected $table = 'program_sale_lists';
+}
