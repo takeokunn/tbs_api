146b4c18aee2e3ae6caaec2d272e1041d8dd6000
TakeObara <bararararatty@gmail.com>
2016-09-17 16:27:39 +0900

    Add 'stock' deal test code

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -41,5 +41,74 @@ class StockTest extends TestCase
             '/api/v1/user/stocks?token=' . $token,
             'success get stocks by user_id'
         );
+
+        /**
+         * deal stocks
+         */
+
+        // error
+        $this->postMethod(
+            '/api/v1/programs/hogehoge/stocks?token=' . $token,
+            'invalid parameter',
+            ['number' => '1', 'type' => 'buy']
+        );
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'invalid argument',
+            ['number' => 'hoge', 'type' => 'buy']
+        );
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'invalid argument',
+            ['number' => '', 'type' => 'buy']
+        );
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'invalid argument',
+            ['number' => 1, 'type' => '']
+        );
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'invalid argument',
+            ['number' => 1, 'type' => 'hoge']
+        );
+        $this->postMethod(
+            '/api/v1/programs/11111/stocks?token=' . $token,
+            'program not exist',
+            ['number' => 1, 'type' => 'buy']
+        );
+
+
+        // buy stocks
+        Artisan::call('db:seed');
+        // success
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'success deal stocks',
+            ['number' => '1', 'type' => 'buy']
+        );
+
+        // failure
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'cannot buy stocks',
+            ['number' => '111', 'type' => 'buy']
+        );
+
+        // sell stocks
+        Artisan::call('db:seed');
+        // success
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'success deal stocks',
+            ['number' => '1', 'type' => 'sell']
+        );
+
+        // failure
+        $this->postMethod(
+            '/api/v1/programs/1/stocks?token=' . $token,
+            'cannot sell stocks',
+            ['number' => '111', 'type' => 'sell']
+        );
     }
 }
